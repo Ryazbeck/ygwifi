77a503a51c12bc47f13177ac91505d7644bf381e
russellyazbeck@gmail.com <russellyazbeck@gmail.com>
2020-08-21 15:09:38 -0500

    might actually work

--- Dockerfile
+++ Dockerfile
@@ -8,12 +8,16 @@ RUN apk add ifupdown dhclient hostapd wireless-tools wpa_supplicant dnsmasq iw
 COPY requirements.txt requirements.txt
 RUN pip3 install -r requirements.txt
 
+RUN sysctl -w net.ipv6.conf.all.disable_ipv6=1
+RUN sysctl -w net.ipv6.conf.default.disable_ipv6=1
+
 RUN mkdir -p /etc/wpa_supplicant/
 RUN mkdir -p /etc/hostapd/
 
-COPY cfg/hostapd.conf /etc/hostapd/hostapd.conf
-COPY cfg/dnsmasq.conf /etc/dnsmasq.conf
-COPY cfg/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
+COPY cfg/hostapd.conf /cfg/hostapd.conf
+COPY cfg/dnsmasq.conf /cfg/dnsmasq.conf
+COPY cfg/wpa_supplicant.conf /cfg/wpa_supplicant.conf
+COPY cfg/interfaces /etc/network/
 
 COPY app /app
 
--- app/__init__.py
+++ app/__init__.py
@@ -24,15 +24,16 @@ handler = logging.handlers.RotatingFileHandler(
 logger.addHandler(handler)
 
 
+# helper funcs
 def run_commands(commands, success_message='Success'):
     for cmd in commands:
         try:
             check_call(cmd)
         except CalledProcessError as e:
             logger.info(e)
-            return Response(f'Failure: {e}', 500)
+            return False
 
-    return Response(f'Success: {success_message}', 200)
+    return True
 
 
 def remove_file(file_path=None):
@@ -49,36 +50,163 @@ def remove_file(file_path=None):
     return True
 
 
-def start_wpa():
-    '''
-    always start wpa
-    if wpa_supplicant is configured then station will connect to wifi
-    else it will allow us to scan for ssids to connect to
-    '''
+def update_wpa_conf(ssid=None, key=None):
+    logger.debug('Updating wpa_supplicant.conf')
 
-    logger.debug('Starting wpa')
+    # hash the key
+    wpa_passphrase = Popen(
+        ['wpa_passphrase', f'"{ssid}"', f'"{key}"'], 
+        universal_newlines=True, stdout=PIPE
+    ).stdout.readlines()
 
-    # sometimes this file hangs. remove it
-    remove_file('/var/run/wpa_supplicant/wlan0')
+    # get hash from output
+    for line in wpa_passphrase:
+        if 'psk' in line and '#' not in line:
+            passphrase = line.strip().replace('psk=','')
+            break
+
+    # this goes in the conf file so we can reuse it
+    wpa_supplicant_conf = f'''
+        country=US
+        ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+        update_config=1
+        p2p_disabled=1
+
+        network={{
+            ssid="{ssid}"
+            psk={passphrase}
+        }}
+        '''
+
+    wpa_supplicant_path = '/cfg/wpa_supplicant.conf'
 
     try:
-        check_call(['wpa_cli', 'status'])
-        logger.debug('wpa already started')
-    except:
-        try:
-            check_call(
-                'wpa_supplicant', 
-                '-B', # background
-                '-i', 'wlan0',
-                '-c', '/etc/wpa_supplicant/wpa_supplicant.conf'
-            )
-        except Exception as e:
-            logger.info(f'wpa_supplicant failed to start: {e}')
+        logger.debug(f'opening {wpa_supplicant_path}')
+        wpa_supplicant = open(wpa_supplicant_path, 'w')
+    except Exception as e:
+        logger.info(f'failed opening: {e}')
+        return False
+
+    try:
+        logger.debug(f'writing {wpa_supplicant_path}')
+        wpa_supplicant.write(wpa_supplicant_conf)
+        return True
+    except Exception as e:
+        logger.info(f'failed writing: {e}')
+        return False
+
+
+def wpa_status();
+    logger.debug('Retrieving wpa_cli status')
+
+    try:
+        wpa_status_out = Popen(['wpa_cli', 'status'], 
+        stdout=PIPE, universal_newlines=True)
+    except Exception as e:
+        logger.info(f'failed to get wpa_cli status: {e}')
+        return False
+
+    wpa_status = {}
+    
+    for fld in wpa_status_out.stdout.readlines()[1::]:
+        field = fld.split('=')
+        wpa_status[field[0]] = field[1].strip()
+
+    logger.debug(f'wpa_cli status: {wpa_status}')
+        
+    return wpa_status
+
+
+def start_wpa():
+    '''
+    Enables scan for ssids
+    If wpa_supplicant is configured station will connect to wifi
+    '''
+
+    logger.debug('Starting wpa_supplicant')
+
+    try:
+        check_call(['killall', 'wpa_supplicant'])
+        check_call([
+            'wpa_supplicant',
+            '-B',
+            '-i', 'wlan1',
+            '-c', '/cfg/wpa_supplicant.conf'
+        ])
+        return True
+    except Exception as e:
+        logger.warning(f'wpa_supplicant failed to start: {e}')
+        return False
+
+
+def get_ip():
+    logger.debug('Requesting IP address')
+
+    try:
+        check_call(['killall', 'dhclient'])
+        check_call(['dhclient', '-v', 'wlan1'])
+        return True
+    except Exception as e:
+        debug.info(f'dhclient failed to get IP: {e}')
+        return False
+
+
+def _connected()
+    connected = os.system('ping -c 1 google.com')
+    if connected == 0:
+        return True
+    return False
+
+
+def _connect(initialize=False):
+    # initialize will return True even if wpa_state isn't COMPLETED
+    logger.debug('Connect to wifi')
+
+    if start_wpa():
+        if wpa_status()['wpa_state'] != 'COMPLETED':
+            # there are no creds or they are wrong
+            logger.info(f'wifi is not authenticated')
+        
+            if initialize:
+                # we can end initialization here and wait for user to update
+                return True
+            return False
+
+        elif not get_ip():
+            logger.critical(f'failed to get IP address')
             return False
 
     return True
 
 
+def _scan():
+    logger.debug('Scanning for ssids')
+
+    try:
+        # the subsequent Popens are pipes to clean the results
+        scan_results = Popen(['iw', 'wlan0', 'scan'], 
+            stdout=PIPE, universal_newlines=True)
+        scan_ssids = Popen(['egrep', 'SSID: \w'], 
+            stdin=scan_results.stdout, stdout=PIPE, universal_newlines=True)
+        scan_ssids = Popen(['awk', '{print $2}'], 
+            stdin=scan_ssids.stdout, stdout=PIPE, universal_newlines=True)
+        ssids = list(set([ssid.strip() for ssid in scan_ssids.stdout.readlines()]))
+        ssids = ", ".join(ssids)
+
+        logger.debug(f'scan results:{ssids}')
+    except Exception as e:
+        logger.warning(f'Failed to get scan results: {e}')
+        return False
+    
+    return set(ssids)
+
+
+def initialize():
+    logger.debug('Initializing ygwifi')
+    _connect(initialize=True)
+
+
+# endpoints
 @app.after_request
 def after_request(response):
     ' Logging after every request. '
@@ -96,10 +224,7 @@ def after_request(response):
 
 @app.route('/apup')
 def apup():
-    logger.debug('turning up ap')
-
-    # sometimes this file sticks around. this will remove if it exists
-    remove_file('/var/run/hostapd/ap0')
+    logger.debug('Enabling AP')
 
     commands = [
         ['iw', 'phy', 'phy0', 'interface', 'add', 'ap0', 'type', '__ap'],
@@ -108,87 +233,50 @@ def apup():
         ['dnsmasq']
     ]
     
-    return run_commands(
-        commands,
-        success_message='AP enabled'
-    )
+    if run_commands(commands):
+        return Response('AP enabled', 200)
+
+    return Response('Failed to enable AP:', 500)
 
 
 @app.route('/apdown')
 def apdown():
-    logger.debug('turning down ap')
+    logger.debug('Disabling AP')
 
     commands = [
         ['ifconfig', 'ap0', 'down'],
-        ['iw', 'dev', 'ap0', 'del'],
         ['killall', 'hostapd', 'dnsmasq']
     ]
     
-    return run_commands(
-        commands,
-        success_message='AP disabled'
-    )
+    if run_commands(commands):
+        return Response('AP disabled', 200)
+
+    return Response('Failed to disable AP', 500)
 
 
 @app.route('/scan')
 def scan():
-    logger.debug('beginning scan for ssids')
+    if wpa_status():
+        ssids = _scan()
+        if ssids:
+            return Response(str(ssids), 200)
 
-    try:
-        check_call(['wpa_cli', 'status'])
-    except CalledProcessError as e:
-        logger.info(f'wpa_cli not started: {e}')
-        start_wpa()
-
-    try:
-        # the subsequent Popens are pipes to clean the results
-        scan_results = Popen(['iw', 'wlan0', 'scan'], 
-            stdout=PIPE, universal_newlines=True)
-        scan_ssids = Popen(['egrep', 'SSID: \w'], 
-            stdin=scan_results.stdout, stdout=PIPE, universal_newlines=True)
-        scan_ssids = Popen(['awk', '{print $2}'], 
-            stdin=scan_ssids.stdout, stdout=PIPE, universal_newlines=True)
-        ssids = list(set([ssid.strip() for ssid in scan_ssids.stdout.readlines()]))
-
-        logger.debug(f'scan results:{", ".join(ssids)}')
-
-        return Response(str(set(ssids)), 200)
-    except Exceptions as e:
-        logger.info(f'scanning failed: {e}')
-
-        return Response(e, 500)
-
-
-
-@app.route('/wlanup')
-def wlanup():
-    logger.debug('turning up wifi')
-
-    commands = [
-        ['ifconfig', 'wlan0', 'up'],
-        ['dhclient', 'wlan0']
-    ]
-    
-    return run_commands(
-        commands,
-        'wlan0 enabled'
-    )
+    return Response('Scan failed', 500)
 
 
 @app.route('/wlandown')
 def wlandown():
     logger.debug('turning down wifi')
     commands = [
-        ['ifconfig', 'wlan0', 'down'],
-        ['wpa_cli', 'terminate'],
-        ['dhclient', '-r'],
-        ['ip', 'addr', 'flush', 'wlan0'],
+        ['ifconfig', 'wlan1', 'down'],
+        ['killall', 'wpa_supplicant', 'dhclient'],
+        ['ip', 'addr', 'flush', 'wlan1'],
     ]
     
-    return run_commands(
-        commands,
-        'wlan0 disabled'
-    )
+    if run_commands(commands):
+        return Response('wlan1 disabled', 200)
+
+    return Response('Failed to disable wlan1', 500)
     
 
 @app.route('/connect')
@@ -196,52 +284,15 @@ def connect():
     ssid = request.args.get('ssid', None)
     key = request.args.get('key', None)
 
-    logger.debug(f'connecting to {ssid}')
-    
     if ssid and key:
-        logger.debug('ssid and key submitted')
-
-        # hash the key
-        wpa_passphrase = Popen(
-            ['wpa_passphrase', f'"{ssid}"', f'"{key}"'], 
-            universal_newlines=True, stdout=PIPE
-        ).stdout.readlines()
-
-        # get hash from output
-        for line in wpa_passphrase:
-            if 'psk' in line and '#' not in line:
-                passphrase = line.strip().replace('psk=','')
-                break
-
-        # this goes in the conf file so we can reuse it
-        wpa_supplicant_conf = f'''
-            country=US
-            ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
-            update_config=1
-            p2p_disabled=1
-
-            network={{
-                ssid="{ssid}"
-                psk={passphrase}
-            }}
-            '''
-
-        # probably unnecessary
-        wpa_supplicant_path = os.environ('WPA_SUPPLICANT_PATH')
-
-        try:
-            logger.debug(f'opening {wpa_supplicant_path}')
-            wpa_supplicant = open(wpa_supplicant_path, 'w')
-        except Exception as e:
-            logger.info(f'failed opening: {e}')
-            return Response(e, 500)
-
-        try:
-            logger.debug(f'writing {wpa_supplicant_path}')
-            wpa_supplicant.write(wpa_supplicant_conf)
-            return Response('Success', 200)
-        except Exception as e:
-            logger.info(f'failed writing: {e}')
+        logger.debug(f'connecting to {ssid}')
+        
+        if not update_wpa_conf(ssid, key):
+            return Response('Failed to update wpa_supplicant.conf', 500)
+        elif not _connect():
+            return Response('Failed to establish connection', 500)
+        else:
+            return Response('connection established', 200)
 
     # missing parameters
     elif ssid and key is None:
@@ -257,12 +308,15 @@ def connect():
 
 @app.route('/connected')
 def connected():
-    response = os.system('ping -c 1 google.com')
-    if response == 0:
+    if _connected():
         return Response('Success', 200)
+
     return Response('Failure', 500)
 
 
 if __name__ == '__main__':
-    start_wpa()
-    app.run()
+    if initialize():
+        app.run()
+    else:
+        logged.critical('Failed to initialize app')
+        exit()
